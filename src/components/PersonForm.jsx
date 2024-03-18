80d896c9689dbfa4a0df7d0ec4e6e3048b81e7f3
Andrei <andrei.paduraru05@gmail.com>
2024-03-18 12:43:00 +0200

    antd form

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -1,81 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { requirePersonById } from "../store/persons/actions";
 import { observer } from "mobx-react-lite";
 import { toJS } from "mobx";
-import { Input, InputNumber, Button, Space } from "antd";
+import { Input, InputNumber, Button, Form } from "antd";
 
 const PersonForm = () => {
   const { id } = useParams(); // Get the selected person's ID from the route params
-
+  const [form] = Form.useForm();
   const pers = requirePersonById(id);
   console.log("pers in form", toJS(pers));
 
-  const [person, setPerson] = useState(null);
-  const [firstName, setFirstName] = useState(pers.firstName);
-
-  const [lastName, setLastName] = useState(pers.lastName);
-  const [score, setScore] = useState(pers.score);
-
-  /* const person = requirePersonById(id); */
-
   useEffect(() => {
-    // Load person data when component mounts or when ID changes
-    setPerson(requirePersonById(id));
-  }, [id]);
+    const person = requirePersonById(id);
+
+    // Set form values when data is loaded
+    form.setFieldsValue({
+      firstName: person.firstName,
+      lastName: person.lastName,
+      score: person.score,
+    });
+  }, [id, form]);
 
   /* console.log("primit din store", person.id, person.lastName); */
 
-  const handleSave = (e) => {
-    e.preventDefault();
-    pers.setFirstName(firstName);
-    pers.setLastName(lastName);
-    pers.setScore(score);
+  const handleSave = (values) => {
+    pers.setFirstName(values.firstName);
+    pers.setLastName(values.lastName);
+    pers.setScore(values.score);
     /* const pers = { firstName, lastName, score }; */
     console.log("saved pers info form", pers);
   };
 
-  // If person data is not loaded yet, return null
-  if (!person) {
-    return null;
-  }
-
-  // Destructure person object to get first name, last name, and score
-  /*   const { firstName, lastName, score } = person; */
-
   return (
-    <form>
-      <label>
-        Name:
-        <Input
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)} // Update name state on change
-        />
-      </label>
-      <label>
-        Surname:
-        <Input
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)} // Update name state on change
-        />
-      </label>
-      <Space>
-        <label>
-          Score:
-          <InputNumber
-            defaultValue={score}
-            min={0}
-            max={100}
-            formatter={(value) => (value !== undefined ? `${value}%` : "")}
-            parser={(value) => value.replace("%", "")}
-            onChange={(value) => setScore(value)}
-          />
-        </label>
-      </Space>
-      <Button type="primary" onClick={handleSave}>
-        SAVE
-      </Button>
-    </form>
+    <Form form={form} onFinish={handleSave}>
+      <Form.Item label="First Name" name='firstName'>
+        <Input />
+      </Form.Item>
+      <Form.Item label="Last Name" name='lastName'>
+        <Input />
+      </Form.Item>
+      <Form.Item label="Score" name='score'>
+        <InputNumber min={0} max={100} />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          SAVE
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
